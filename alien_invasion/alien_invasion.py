0bd0b304c0aeeea135342377c03393050c5dfa5e
guilherme.castilho <guilherme.castilho@semantix.com.br>
2022-06-10 17:10:04 -0300

    alien_invasion-limitando_alcance_espaconave

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -13,7 +13,7 @@ def run_game():
 	pygame.display.set_caption("Alien Invasion")
 	
 	# Cria uma espaçonave
-	ship = Ship(screen)
+	ship = Ship(ai_settings, screen)
 	
 	# Define a cor de fundo
 	bg_color = (230, 230, 230)
@@ -21,7 +21,8 @@ def run_game():
 	# Inicia o laço principal do jogo
 	while True:
 		# Observa eventos 
-		gf.check_events()
+		gf.check_events(ship)
+		ship.update()
 		gf.update_screen(ai_settings, screen, ship)
 		
 run_game()
