9a08acb0a6432b3616e997ff50f4c58afe049f9e
guilherme.castilho <guilherme.castilho@semantix.com.br>
2022-06-11 15:53:16 -0300

    alien_invasion-bullets_limited_for_tree_fires

--- alien_invasion/game_functions.py
+++ alien_invasion/game_functions.py
@@ -11,8 +11,9 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
 		ship.moving_left = True
 	elif event.key == pygame.K_SPACE:
 		# Cria um nobo projétil e o adiciona ao grupo de projéteis
-		new_bullet = Bullet(ai_settings, screen, ship)
-		bullets.add(new_bullet)
+		if len(bullets) < ai_settings.bullets_allowed:
+			new_bullet = Bullet(ai_settings, screen, ship)
+			bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -45,10 +46,16 @@ def update_screen(ai_settings, screen, ship, bullets):
 	# Deixa a tela mais recente visível
 	pygame.display.flip()
 	
-
-	
-	
-	
+def update_bullets(bullets):
+	"""Atualiza a posição dos projéteis e se livra dos projéteis"""
+	# Atualiza as posições dos projéteis
+	bullets.update()
+		
+	# Livra-se dos projéteis que desapareceram
+	for bullet in bullets.copy():
+		if bullet.rect.bottom <= 0:
+			bullets.remove(bullet)
+		
 	
 	
 	
